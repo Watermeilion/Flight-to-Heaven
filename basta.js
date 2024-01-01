eee841855d5a293873a34f8f89a809084c48bd5f
Watermeilion <153797307+Watermeilion@users.noreply.github.com>
2024-01-01 14:21:56 +0800

    Add files via upload

--- basta.js
+++ basta.js
@@ -1,14 +1,36 @@
-Array.from(document.getElementsByTagNamem ('input')).forEach((e,i)=>{
-    e.addEventListener('keyup',(ei)=>{
-      if(e.value.length > 0 ){
-        document.getElementsByClassName('bi')[i].getElementsByClassName.transform="rotate(180deg)";
-      } else {
-        document.getElementsByClassName('bi')[i].getElementsByClassName.transform="rotate(0deg)";
-      }
-    })
-})
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev'); // Corrected from 'querySelectorId' to 'getElementById'
+let next = document.getElementById('next'); // Corrected from 'querySelectorId' to 'getElementById'
 
-app.get('/',(req, res) => {
-  res.sendFile(__dirname+'/indexafrica.html')
+let active = 0;
+let lengthItems = items.length - 1;
 
-})
+next.onclick = function() {
+  if (active + 1 > lengthItems) {
+    active = 0;
+  } else {
+    active = active + 1;
+  }
+  reloadSlider();
+}
+
+prev.onclick = function() {
+  if (active - 1 < 0) {
+    active = lengthItems;
+  } else {
+    active = active - 1;
+  }
+  reloadSlider();
+}
+
+function reloadSlider() {
+  let checkLeft = items[active].offsetLeft;
+  list.style.left = -checkLeft + 'px';
+
+  let lastActiveDot = document.querySelector('.slider .dots li.active');
+      lastActiveDot.classList.remove('active');
+      dots[active].classList.add('active');
+}
+
